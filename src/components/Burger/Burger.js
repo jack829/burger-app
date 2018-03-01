4259e2d2b01603b0cf11592452cbccca0e8d7923
Jack Peterson <jlpeterson829@gmail.com>
2018-03-01 15:58:10 -0500

    Add basic passing of ingredients from the burger builder

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,10 +3,27 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css';
 
 const burger = (props) => {
+  const allIngredients = Object.keys(props.ingredients).map((ingredient) => {
+    // Old way
+    // let i = props.ingredients[ingredient]
+    // const ings = [];
+    // while (i) {
+    //   ings.push(<BurgerIngredient key={ingredient + 1} type={ingredient} />);
+    //   i--;
+    // }
+    // return ings;
+    // -----------------
+    // New way
+    // Create a new array whose length is the value of this object key
+    return [...Array(props.ingredients[ingredient])].map((_, i) => {
+      return <BurgerIngredient key={ingredient + 1} type={ingredient} />
+    });
+  });
+
   return (
     <div className={classes.Burger}>
-      {/* {props.ingredients.map((ingredient) => <BurgerIngredient type={ingredient.type} /> )} */}
       <BurgerIngredient type="bread-top" />
+      {allIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   )
