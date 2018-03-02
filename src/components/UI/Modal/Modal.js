60087df205d73378544d2bcf7fff9a3b21b156b8
Jack Peterson <jlpeterson829@gmail.com>
2018-03-02 16:28:42 -0500

    Only re-render the modal when necessary

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './Modal.css';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => {
-  return (
-    <Aux>
-      <Backdrop show={props.show} clicked={props.closeModal} />
-      <div
-        className={styles.Modal}
-        // Animate the modal into view when show property is "true"
-        style={{
-          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: props.show ? '1' : '0'
-        }}>
-          {props.children}
-      </div>
-    </Aux>
-  );
-};
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
 
-export default modal;
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.closeModal} />
+        <div
+          className={styles.Modal}
+          // Animate the modal into view when show property is "true"
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}>
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
+
+export default Modal;
